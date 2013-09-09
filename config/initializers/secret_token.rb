39773d65bbede212690890825d56c1483ff3ccce
Ali Rushdan Tariq <alirtariq@gmail.com>
2013-09-09 14:29:52 -0400

    created scaffold and pages controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lastfav::Application.config.secret_key_base = 'f73bfe37e2e59cc29b78f5623af4ae9b33781fb5cf1083fcf7ecf296b4aa2a6075a53280953c2d0100041641f894f0a7a833eaaa36240187f8f8b9976a8d0b8b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lastfav::Application.config.session_store :cookie_store, key: '_lastfav_session'
